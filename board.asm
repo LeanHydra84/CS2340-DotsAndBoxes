5b6e011c01cd8b435add0ff856780f163b5d3b2a
unknown <guslindell2@gmail.com>
2023-04-26 18:53:04 -0500

    update

--- board.asm
+++ board.asm
@@ -1,7 +1,8 @@
     .data
 
 board: .space 64
-dotstr: .asciiz "  .  "
+dotstr: .asciiz " . "
+hdash: .asciiz "--"
 halfspacer: .asciiz "  "
 threespacer: .asciiz "   "
 
@@ -13,31 +14,34 @@ threespacer: .asciiz "   "
 ## $t0 : Array indexer
 ## $t1 : Horizontal Line counter
 ## $t2 : Vertical Height (NOT the same as tile height)
-## $t3 : 
 ## $t4 : Vertical Line counter
+## $t3 : 
 ###
 
 draw_board:
-
+	# TEST CODE
 	la $t0, board
-	li $t1, 16
+	li $t1, 8
 	
 	sb $t1, 0($t0)
 	sb $t1, 5($t0)
 	sb $t1, 6($t0)
+	sb $t1, 63($t0)
+	# END TEST CODE
 
 	# save return address
     subi $sp, $sp, 4
     sw $ra, ($sp)
     
-    jal print_abcs
+	jal print_abcs
 
-    # $s0 contains dimension
+	# $s0 contains dimension
 
-    li $t0, 0 # Array indexer
-    li $t1, 0 # Horizontal line counter
-    li $t4, 0 # Vertical line counter
+	li $t0, 0 # Array indexer
+	li $t1, 0 # Horizontal line counter
+	li $t4, 0 # Vertical line counter
 
+	# number of total rows drawn (tiles + dots)
 	mul $t2, $s0, 2
 	addi $t2, $t2, 1
 	
@@ -50,6 +54,36 @@ db_loop_pt:
     li $v0, 4 # print char
     la $a0, dotstr
     syscall
+    
+    # GET LINE LEFT->RIGHT from below tile
+    add $t5, $t0, $t1 # Get address of below tile
+    subi $t6, $t2, 1
+    bne $t4, $t6, db_horiz_skip_edgecase # check if last row
+    
+    # EDGE CASE
+    sub $t5, $t5, $s0 # subtract row size to get tile above
+    lb $t5, board($t5)
+    andi $t5, $t5, 4 # 0100
+    beqz $t5, db_draw_horiz_blank
+    j db_draw_horiz_dash
+    
+db_horiz_skip_edgecase:
+    lb $t5, board($t5)
+	andi $t5, $t5, 8 # 1000
+	beqz $t5, db_draw_horiz_blank
+	
+db_draw_horiz_dash:
+	# Draw horizontal dash
+	li $v0, 4
+	la $a0, hdash
+	syscall
+	
+	j db_process_eol
+	
+db_draw_horiz_blank:
+	li $v0, 4
+	la $a0, halfspacer
+	syscall
 
     j db_process_eol
     
@@ -74,26 +108,34 @@ db_get_val:
 	la $a0, halfspacer
 	syscall
     
-db_process_eol:
+db_process_eol: # checks for end of line, adds newline + other stuff
     # New Lines
     addi $t1, $t1, 1
     
     blt	$t1, $s0, db_skipnewline
+    addi $t4, $t4, 1
+    andi $t5, $t4, 0x1 # Mask bit for odd numbers
+    beqz $t5, db_printnewline
     
+    li $v0, 4
+    la $a0, dotstr
+    syscall
+    
+db_printnewline:
     li $t1, 0
     li $v0, 11 # print char
     li $a0, 0xA # Newline ascii code
     syscall
+
     
-    addi $t4, $t4, 1
-    
-    andi $t5, $t4, 1 # Mask bit for odd numbers
+
     beqz $t5, db_printlinenumber
     
     # Align symbols
     li $v0, 4
     la $a0, threespacer
 	syscall
+	
 	j db_skipnewline
 	
 db_printlinenumber:
@@ -115,6 +157,7 @@ db_exit:
     lw $t0, ($sp)
     addi $sp, $sp, 4
     jr $t0
+# END DRAW FUNCTION
 
 
 # Function to get the capture symbol for a given tile byte
@@ -131,6 +174,7 @@ bgcc_comp:
 bgcc_pl:
 	li $v0, 120 # x ascii value
 	jr $ra
+# END GET_CAPTURE_CHAR FUNCTION
 
 
 # Print A B C
@@ -159,12 +203,44 @@ pabc_loop:
 	addi $t1, $t1, 1
 	addi $t0, $t0, 1
 	blt $t0, $s0, pabc_loop
+
+	# end pabc_loop
 	
+	# trailing newline
 	li $v0, 11
 	li $a0, 10
 	syscall
 	
 	jr $ra
+# END PRINT_ABC FUNCTION
+
+set_line_between:
+	# $a0 : dot 1 x
+	# $a1 : dot 1 y
+	# $a2 : dot 2 x
+	# $a3 : dot 2 y
+	
+	beq $a0, $a2, slb_vline # if both x values are the same, the line is vertical
+slb_hline:
+	
+	# Top address = board offset: (d1X + s0 * minY)
+	# Bottom address = board offset: (d1X + s0 * maxY)
+	
+	# get minY
 	
 	
+	mul $t1, $s0, #MINY
+	
+	
+	
+	jr $ra
+slb_vline:
+
+	# Left address = board offset: (s0 * d1Y + minX)
+	# Right address = board offset: (s0 * d1Y + maxX)
+	jr $ra
+
+# END SET_LINE_BETWEEN FUNCTION
+
+
 	
